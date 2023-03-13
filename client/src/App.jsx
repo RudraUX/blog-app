4f951816dffaab2e2b0d9139ce7e784ca0ef1495
RudraUX <pollabrock@gmail.com>
2023-03-13 16:02:44 +0600

    ✨build fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Register, Login, Single, Write, NotFound, Home, Post } from './pages';
+import { Navbar, Footer } from './components';
+import './style.scss';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Single } from './Single';
+export { default as Write } from './Write';
+export { default as NotFound } from './NotFound';
+export { default as Home } from './Home';
+export { default as Post } from './Post';
