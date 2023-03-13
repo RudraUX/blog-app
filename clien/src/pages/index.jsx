3c2a10b40e4ac85c4fff17738ed7fb5faa58b365
RudraUX <pollabrock@gmail.com>
2023-03-13 15:30:15 +0600

    test

---
+++ clien/src/pages/index.jsx
@@ -0,0 +1,6 @@
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Single } from './Single';
+export { default as Write } from './Write';
+export { default as NotFound } from './NotFound';
+export { default as Home } from './Home';
