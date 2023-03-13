dee5ada9b7fd77fab74b09aa00b0f963e4c1ebaa
RudraUX <pollabrock@gmail.com>
2023-03-13 16:35:10 +0600

    ✨test api added

--- server/app.js
+++ server/app.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    status: 'success',
+    data: {
+      id: 1,
+      name: 'John Doe',
+      email: '',
+      phone: '',
+      address: '',
+      city: '',
+      state: '',
+      zip: '',
+      country: '',
+      created_at: new Date(Date.now()),
+      updated_at: '',
+    },
+  });
 });
 
 app.listen(3000, () => {
